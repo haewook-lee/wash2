98b12b8e8a651a499d1b68ed2b50acb89f9f9e9b
haewook-lee <20hwlee@gmail.com>
2023-09-12 00:56:20 -0400

    i

--- data/merge.js
+++ data/merge.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Directory path where your JSON files are located
-const directoryPath = './dataset'; // Replace with the actual directory path
+const directoryPath = './dataset';
 
 // Function to read and merge JSON files in a directory
 function mergeJSONFiles(dirPath) {
@@ -19,8 +19,6 @@ function mergeJSONFiles(dirPath) {
         // Read and parse the JSON file
         const fileData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-        // Merge the data from this file into the mergedData object
-        // Object.assign(mergedData, fileData);
         mergedData.push(...fileData)
 
         console.log(`Merged data from ${filePath}`);
@@ -39,8 +37,7 @@ const mergedData = mergeJSONFiles(directoryPath);
 // Convert the merged data object to a JSON string
 const mergedDataJSON = JSON.stringify(mergedData, null, 2); // 2-space indentation for formatting
 
-// Define the output file path
-const outputPath = './merged_data.json'; // Replace with your desired output path
+const outputPath = './merged_data.json';
 
 // Write the merged data to a single JSON file
 fs.writeFileSync(outputPath, mergedDataJSON, 'utf-8');
--- data/scrape.js
+++ data/scrape.js
@@ -1,15 +1,11 @@
 const axios = require('axios');
-const fs = require('fs'); // Import the 'fs' module
+const fs = require('fs');
 
-// Set the API endpoint URL
 const apiUrl = 'https://www.refugerestrooms.org/api/v1/restrooms';
-
-// Set the rate limit (requests per minute)
-const rateLimit = 60;
 
 // Set the total number of pages to fetch
 // Max pages is 535
-const totalPages = 535; // Adjust this based on your needs
+const totalPages = 535;
 
 // Set the number of records per page
 const recordsPerPage = 100;
@@ -20,11 +16,8 @@ let currentPage = 503;
 // Function to fetch data for the current page
 async function fetchData() {
   try {
-    // Calculate the offset for the current page
-    // const offset = (currentPage - 1) * recordsPerPage;
     const offset = 0
 
-    // Make the API request for the current page
     const response = await axios.get(apiUrl, {
       params: {
         page: currentPage,
@@ -39,12 +32,11 @@ async function fetchData() {
     // Save the data to a JSON file
     saveDataToFile(response.data);
 
-    // Increment the current page
     currentPage++;
 
     // If there are more pages to fetch, schedule the next request
     if (currentPage <= totalPages) {
-      setTimeout(fetchData, 45000); // Sleep for 60 seconds (1 minute)
+      setTimeout(fetchData, 45000);
     }
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -55,8 +47,7 @@ async function fetchData() {
 function saveDataToFile(data) {
   const jsonData = JSON.stringify(data, null, 2); // Convert data to JSON format with 2-space indentation
 
-  // Define the file path where you want to save the JSON data
-  const filePath = `./output-${currentPage}.json`; // Change this to your desired file path
+  const filePath = `./output-${currentPage}.json`;
 
   // Write the JSON data to the file
   fs.writeFile(filePath, jsonData, 'utf8', (err) => {
@@ -68,5 +59,4 @@ function saveDataToFile(data) {
   });
 }
 
-// Start fetching data
 fetchData();

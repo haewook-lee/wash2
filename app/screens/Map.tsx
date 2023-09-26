a3d3186ca5fe9f90503807a189e5035947aa4de4
haewook-lee <20hwlee@gmail.com>
2023-09-26 14:33:31 -0400

    user location

--- app/screens/Map.tsx
+++ app/screens/Map.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { DocumentData, collection, doc, getDocs } from "firebase/firestore"; 
 import { FIRESTORE_DB } from '../../FirebaseConfig';
+import * as Location from 'expo-location';
 
 interface MarkerData {
   id: string;
@@ -12,13 +13,18 @@ interface MarkerData {
   directions: string;
 }
 
+interface userLoc {
+  latitude: number;
+  longitude: number
+}
+
 export default function Map() {
   const [markers, setMarkers] = useState<DocumentData[]>([]);
+  const [userLocation, setUserLocation] = useState<userLoc | null>(null)
 
   const washroomsRef = collection(FIRESTORE_DB, 'washroom')
 
   useEffect(() => {
-
     // Fetch data from Firestore and update markers state
     const fetchData = async () => {
       try {
@@ -38,10 +44,40 @@ export default function Map() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Fetch user's location
+    (async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.error('Permission to access location was denied');
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        setUserLocation({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        });
+      } catch (error) {
+        console.error('Error fetching user location:', error);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={{ latitude: 36.8712433, longitude: -76.4412966, latitudeDelta: 0.1, longitudeDelta: 0.1 }}>
-        
+      {userLocation &&
+      <MapView style={styles.map} initialRegion={{ latitude: userLocation.latitude, longitude: userLocation.longitude, latitudeDelta: 0.1, longitudeDelta: 0.1 }}>
+        {/* user marker */}
+        <Marker
+            coordinate={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude,
+            }}
+            title="Your Location"
+            pinColor="blue"
+          />
         {markers.map((marker) => (
           <Marker
             key={marker.id}
@@ -50,7 +86,7 @@ export default function Map() {
             description={marker.directions}
           />
         ))}
-      </MapView>
+      </MapView>}
     </View>
   );
 }
